92b1504832b20f6a5b497931870a2c3521216546
Namhkn12 <vutuannam39@gmail.com>
2026-01-20 18:13:28 +0700

    Initial commit

---
+++ src/main/java/com/roguesmp/RogueSmpCore.java
@@ -0,0 +1,17 @@
+package com.roguesmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RogueSmpCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
