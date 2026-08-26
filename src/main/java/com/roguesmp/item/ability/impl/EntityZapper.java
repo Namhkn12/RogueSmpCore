b6f90e464b24ab91fb6f9e01e1b0afd1eacf5011
Namhkn12 <vutuannam39@gmail.com>
2026-08-26 11:15:52 +0700

    feat: EntityZapper item ability.

---
+++ src/main/java/com/roguesmp/item/ability/impl/EntityZapper.java
@@ -0,0 +1,54 @@
+package com.roguesmp.item.ability.impl;
+
+import com.roguesmp.codec.Codec;
+import com.roguesmp.item.SmpItem;
+import com.roguesmp.item.ability.ItemAbility;
+import com.roguesmp.player.SmpPlayer;
+import com.roguesmp.utils.MovementUtils;
+import com.roguesmp.utils.Utils;
+import io.papermc.paper.registry.keys.SoundEventKeys;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class EntityZapper implements ItemAbility {
+
+    public static final Codec<EntityZapper> CODEC = Codec.BOOLEAN.xmap(aBoolean -> new EntityZapper(), EntityZapper::isFlag);
+    public static final String TYPE_KEY = "entity_zapper";
+
+    @Override
+    public String getTypeId() {
+        return TYPE_KEY;
+    }
+
+    @Override
+    public @Nullable List<Component> getDisplay(SmpPlayer player, SmpItem smpItem) {
+        return List.of(Utils.text("Chuột phải để xóa entity khỏi world.", NamedTextColor.RED, TextDecoration.BOLD));
+    }
+
+    @Override
+    public String getSimpleDescription() {
+        return "Vật phẩm có thể dùng để xóa entity khỏi world.";
+    }
+
+    @Override
+    public void onInteractEntity(SmpPlayer player, SmpItem item, PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof Player player1) {
+            event.getPlayer().sendMessage(Component.text("NO!", NamedTextColor.RED));
+            event.getPlayer().playSound(Sound.sound(SoundEventKeys.ENTITY_WIND_CHARGE_WIND_BURST, Sound.Source.PLAYER, 1f, 1.2f));
+            MovementUtils.knockAway(player1, event.getPlayer(), 1f);
+            return;
+        }
+        event.getRightClicked().remove();
+    }
+
+    public boolean isFlag() {
+        return true;
+    }
+}
