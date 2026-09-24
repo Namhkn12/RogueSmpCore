9d66f07c68328a08f902ac551326370189dc7b0a
Namhkn12 <vutuannam39@gmail.com>
2026-09-24 19:25:40 +0700

    fix: SplitArrow bounce more than expected (+1)

--- src/main/java/com/roguesmp/player/ability/impl/archer/SplitArrow.java
+++ src/main/java/com/roguesmp/player/ability/impl/archer/SplitArrow.java
@@ -46,10 +46,9 @@ public class SplitArrow extends Ability {
         if (event.getDamageType() != DamageType.PROJECTILE || !(event.getDamager() instanceof Projectile) || EntityUtils.isFriendly(event.getDamager())) return;
         if (!(event.getVictim() instanceof LivingEntity living)) return;
         double damage = event.getFinalDamage() * multiplier;
-        int count = 1 + bounce;
         LivingEntity sourceEnemy = living;
         List<LivingEntity> chainedMobs = new ArrayList<>();
-        for (int j = 0; j < count; j++) {
+        for (int j = 0; j < bounce; j++) {
             chainedMobs.add(sourceEnemy);
             List<LivingEntity> nearbyMobs = EntityUtils.getNearbyMobs(sourceEnemy.getLocation(), range, range, range, living1 -> !chainedMobs.contains(living1));
             LivingEntity nearestMob = EntityUtils.getNearestMob(sourceEnemy.getLocation(), nearbyMobs);
